20e3aefda0d6c57183732a82d8f723926ba5faf7
bhushanp <32756967+bhushanpawade@users.noreply.github.com>
2018-06-01 22:11:10 +0530

    added babelrc

--- typescript/src/lambdas/helloWorld/index.ts
+++
@@ -1,9 +0,0 @@
-import "babel-polyfill";
-
-/**
- * Example hello world Lambda function.  This is the function AWS will invoke.
- */
-export async function handler(evt: any): Promise<string> {
-    // Write biz logic and get paid.
-    return "Hello world";
-}
