34c47e9f54ae19aba82870528a2812a5b48fbafd
bhushanp <32756967+bhushanpawade@users.noreply.github.com>
2018-06-04 16:57:52 +0530

    updated files

--- javascript/src/lambdas/helloWorld/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Example hello world Lambda function.  This is the function AWS will invoke.
- */
-export async function handler(evt) {
-    // Write biz logic and get paid.
-    return "Hello world";
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,72 +2,66 @@ const fs = require('fs');
 const path = require('path');
 const ZipPlugin = require('zip-webpack-plugin');
 
-module.exports = function (env) {
-    const lambdaFunctionDir = path.join(__dirname, 'src', 'lambdas');
-    const functionsToBuild = env && env.fxn ? env.fxn.split(",") : fs.readdirSync(lambdaFunctionDir).filter(item => fs.lstatSync(path.join(lambdaFunctionDir, item)).isDirectory() && !item.match(/^\./));
-    console.log(`Building ${functionsToBuild.join(", ")}`);
+module.exports = {
 
-    return functionsToBuild
-        .map(fxn => ({
-            mode: 'production',
-            context: path.resolve(__dirname),
-            //entry: path.join(lambdaFunctionDir, fxn, 'index.js'),
-            entry: ["babel-polyfill", path.join(lambdaFunctionDir, fxn, 'index.js')],
-
-            output: {
-                path: path.join(__dirname, 'dist', fxn),
-                filename: 'index.js',
-                libraryTarget: 'commonjs2'
-            },
-            module: {
-                rules: [
+    //entry: path.join(lambdaFunctionDir, fxn, 'index.js'),
+    entry: {
+       HelloWorld : ["babel-polyfill", path.join(__dirname,'src/lambdas/helloWorld/index.js')],
+       Robots : ["babel-polyfill", path.join(__dirname,'src/lambdas/robots/index.js')]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
                     {
-                        test: /\.js$/,
-                        use: [
-                            {
-                                loader: 'babel-loader',
-                                options: {
-                                    presets: ['es2015'],
-                                    plugins: ["transform-async-to-generator"],
-                                    compact: false,
-                                    babelrc: true
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-                        use: [
-                            'file-loader'
-                        ]
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015'],
+                            plugins: ["transform-async-to-generator"],
+                            compact: false,
+                            babelrc: true
+                        }
                     }
                 ]
             },
-            optimization: {
-                minimize: false,
-                namedModules: true
-            },
-            plugins: [
-                new ZipPlugin({
-                    path: path.join(__dirname, 'dist', fxn),
-                    pathPrefix: '',
-                    filename: `${fxn}.zip`
-                })
-            ],
-            target: 'node',
-            externals: {
-                // These modules are already installed on the Lambda instance.
-                'aws-sdk': 'aws-sdk',
-                'awslambda': 'awslambda',
-                'dynamodb-doc': 'dynamodb-doc',
-                'imagemagick': 'imagemagick'
-            },
-            node: {
-                // Allow these globals.
-                __filename: false,
-                __dirname: false
-            },
-            stats: 'errors-only',
-            bail: true
-        }));
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimize: false,
+        namedModules: true
+    },
+    plugins: [
+        new ZipPlugin({
+            path: path.join(__dirname, 'dist'),
+            pathPrefix: '',
+            filename: `[name].zip`
+        })
+    ],
+    target: 'node',
+    externals: {
+        // These modules are already installed on the Lambda instance.
+        'aws-sdk': 'aws-sdk',
+        'awslambda': 'awslambda',
+        'dynamodb-doc': 'dynamodb-doc',
+        'imagemagick': 'imagemagick'
+    },
+    node: {
+        // Allow these globals.
+        __filename: false,
+        __dirname: false
+    },
+    stats: 'errors-only',
+    bail: true
 };
